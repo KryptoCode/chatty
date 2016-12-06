3b73ec2068a1d013385794a9cdc8ed1e0822110b
Rob DelValle <robdel13@gmail.com>
2016-12-06 15:48:41 -0700

    get and post implimented

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var app = express();
+var messages = [];
+
+app.use(express.static('assets'))
+app.use(bodyParser.json())
+
+app.get('/messages', function(request, res, next) {
+	res.status(200).json({ messages: messages });
+});
+
+app.post('/messages', function(request, res, next) {
+	messages.push(request.body.message);
+	res.status(200).json({ messages: messages });
+});
+
+app.listen(3000);
+
 
